303b40270d0d5b3dead4db4209e6820d4c1d9f96
Arvind Gouttumukkala <go_arvind@berkeley.edu>
2025-02-22 11:17:07 -0800

    flushed out structural similarity function - AG

--- StructSim.py
+++ StructSim.py
@@ -1,7 +1,17 @@
-from typing import List, Dict, Iterable
 import RNAFold
 
-def StructSim(s: str, F: List[str]):
-    RNAf = [RNAFold.RNAFold(f) for f in F]
+
+def StructSim(s: str, F: list[str]) -> float:
+    RNAF = [RNAFold.RNAFold(f) for f in F]
     RNAs = RNAFold.RNAFold(s)
-    pass
+
+    total_index = 0
+    for RNAf in RNAF:
+        intersect = []
+        for pair in RNAs:
+            if pair in RNAf:
+                intersect.append(pair)
+        j_index = len(intersect) / (len(RNAs) + len(RNAf) - len(intersect))
+        total_index += j_index
+
+    return total_index
