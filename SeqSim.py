6b4e4a7e7fdbe42ce4da11fa6712e13b00b72f3a
Arvind Gouttumukkala <go_arvind@berkeley.edu>
2025-02-26 23:44:36 -0800

    changed to fitting alignment - AG

--- SeqSim.py
+++ SeqSim.py
@@ -1,34 +1,67 @@
 from typing import List, Dict, Iterable, Tuple
 import numpy as np
 
-def local_alignment(match_reward: int, mismatch_penalty: int, indel_penalty: int, s: str, t: str):
+
+def fitting_alignment(s: str, t: str, match_reward: int, mismatch_penalty: int, indel_penalty: int) -> Tuple[int, str, str]:
     """
-    Compute the local alignment of two strings based on match reward, mismatch penalty, and indel penalty.
+    Compute the fitting alignment of two strings given a match reward, mismatch penalty, and indel penalty.
+    String t is fit to s (all of t is aligned to s)
     """
-    score = np.zeros((len(s)+1, len(t)+1))
-    
-    max_score = 0
-    max_pos = (0,0)
-    
-    for i in range(1,len(s)+1):
-        for j in range(1,len(t)+1):
-            if s[i-1] == t[j-1]:
-                match = match_reward
-            else:
-                match = -mismatch_penalty
-                
-            score[i,j] = max(score[i-1, j]-indel_penalty, score[i,j-1]-indel_penalty, score[i-1,j-1]+match, 0)
-            if score[i,j] > max_score:
-                max_score = score[i,j]
-                max_pos = (i,j)
-     
-    i = max_pos[0]
-    j = max_pos[1]
-    return score[i,j]
+    cache = {}
+
+    def count_and_path(i, j):
+        if (i, j) in cache:
+            return cache[(i, j)]
+        max_count = float('-inf')
+        max_first = ""
+        max_second = ""
+        if i == 0 and j == 0:
+            max_count = 0
+        else:
+            if i == len(s) and j == len(t):
+                for k in range(len(s) + 1):
+                    if k == len(s) and j == len(t):
+                        continue
+                    count, first, second = count_and_path(k, j)
+                    if count >= max_count:
+                        max_count = count
+                        max_first = first
+                        max_second = second
+            elif j == 0:
+                max_count = 0
+            if i != 0 and j != 0:
+                diag_count, diag_first, diag_second = count_and_path(i - 1, j - 1)
+                match_score = match_reward if s[i - 1] == t[j - 1] else 0 - mismatch_penalty
+                diag_count += match_score
+                if diag_count > max_count:
+                    max_count = diag_count
+                    max_first = diag_first + s[i - 1]
+                    max_second = diag_second + t[j - 1]
+            if i != 0:
+                down_count, down_first, down_second = count_and_path(i - 1, j)
+                down_count -= indel_penalty
+                if down_count > max_count:
+                    max_count = down_count
+                    max_first = down_first + s[i - 1]
+                    max_second = down_second + '-'
+            if j != 0:
+                right_count, right_first, right_second = count_and_path(i, j - 1)
+                right_count -= indel_penalty
+                if right_count >= max_count:
+                    max_count = right_count
+                    max_first = right_first + '-'
+                    max_second = right_second + t[j - 1]
+
+        info = (max_count, max_first, max_second)
+        cache[(i, j)] = info
+        return info
+    info = count_and_path(len(s), len(t))
+    return info
 
 
 def SeqSim(s: str, F: List[str], match_reward: int = 1, mismatch_penalty: int = 1, indel_penalty: int = 1) -> int:
     score = 0
     for f in F:
-        score += local_alignment(match_reward, mismatch_penalty, indel_penalty, s, f)
+        curr_score, f_align, s_align = fitting_alignment(f, s, match_reward, mismatch_penalty, indel_penalty)
+        score += curr_score
     return score
