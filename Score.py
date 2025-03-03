e033d3cd3f892eea3baf8b92a92d95e93e5e6bd0
Arvind Gouttumukkala <go_arvind@berkeley.edu>
2025-03-03 10:58:25 -0800

    formatting - AG

--- Score.py
+++ Score.py
@@ -1,7 +1,6 @@
-from typing import List, Dict, Iterable
 import StructSim
 import SeqSim
 
 
-def Score(s: str, F: List[str], a = 1, b = 1):
+def Score(s: str, F: list[str], a: int = 1, b: int = 1) -> float:
     return a * SeqSim.SeqSim(s, F) + b * StructSim.StructSim(s, F)
