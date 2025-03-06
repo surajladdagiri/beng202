b69d10812a52e0765ca29ae9843c8572668aeaba
Arvind Gouttumukkala <go_arvind@berkeley.edu>
2025-03-05 16:41:24 -0800

    updated structural similarity function - AG

--- StructSim.py
+++ StructSim.py
@@ -6,18 +6,23 @@ def StructSim(s: str, F: list[str]) -> float:
     Function that calculates the structural similarity between an RNA sequence and a list of RNA sequences
     :param s: an RNA sequence
     :param F: a set of RNA sequences
-    :return: the sum of the Jaccard indices between s and each RNA sequence in F
+    :return: the sum of the max Jaccard index between each secondary structures of s and each secondary structure of f
+    in F
     """
-    RNAF = [RNAFold(f) for f in F]
-    RNAs = RNAFold(s)
 
     total_index = 0
-    for RNAf in RNAF:
-        intersect = []
-        for pair in RNAs:
-            if pair in RNAf:
-                intersect.append(pair)
-        j_index = len(intersect) / (len(RNAs) + len(RNAf) - len(intersect))
+    RNAss = RNAFold(s)
+    for f in F:
+        j_index = 0
+        RNAfs = RNAFold(f)
+        for RNAf in RNAfs:
+            for RNAs in RNAss:
+                intersect = []
+                for pair in RNAs:
+                    if pair in RNAf:
+                        intersect.append(pair)
+                curr_j = len(intersect) / (len(RNAs) + len(RNAf) - len(intersect))
+                j_index = max(j_index, curr_j)
         total_index += j_index
 
     return total_index
