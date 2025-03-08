3dec8bee6ec26379dca4d6fcaba5e0027f349fdd
Arvind Gouttumukkala <go_arvind@berkeley.edu>
2025-03-07 16:11:40 -0800

    changes to match problem formulation - AG

--- main.py
+++ main.py
@@ -2,47 +2,32 @@
 import Score
 
 fluorescent_seqs = []
+genes = []
 
 if fluorescent_seqs:
     k = len(fluorescent_seqs[0])
 else:
-    raise RuntimeError("Please pass in some input sequences!")
+    raise RuntimeError("Please pass in some fluorescent sequences!")
 
 for i in range(len(fluorescent_seqs)):
     if fluorescent_seqs[i] != k:
-        raise RuntimeError("All input sequences must be the same length!")
+        raise RuntimeError("All fluorescent sequences must be the same length!")
 
+if not genes:
+    raise RuntimeError("Please pass in some gene sequences!")
 
-def kmer_set(k: int) -> list[str]:
-    """
-    Generates all possible RNA nucleotide kmers of a given length k
-    :param k: length of kmers to generate
-    :return: list of all possible RNA kmers
-    """
-    nucs = ['A', 'U', 'C', 'G']
-    kmers = []
-    if k == 0:
-        return kmers
-    else:
-        sub_kmers = kmer_set(k - 1)
-        for kmer in sub_kmers:
-            new_kmers = [n + kmer for n in nucs]
-            kmers += new_kmers
-        return kmers
-
-
-best_kmers = []
-best_score = float('inf')
-
-for kmer in kmer_set(k):
-    curr_score = Score.Score(kmer, fluorescent_seqs)
+best_score = float('-inf')
+best_gs = []
+for i, g in enumerate(genes, start=1):
+    curr_score = Score.Score(g, fluorescent_seqs)
+    print(f"Score for gene {i}: {curr_score}")
     if curr_score > best_score:
         best_score = curr_score
-        best_kmers = [kmer]
+        best_gs = [i]
     elif curr_score == best_score:
-        best_kmers.append(kmer)
+        best_gs.append(i)
 
 print("Best overall score: {}".format(best_score))
-print("\nKmers with that score: ")
-for kmer in best_kmers:
-    print(kmer)
+print("\nGenes with that score: ")
+for g_i in best_gs:
+    print(g_i)
