54d0e90900d8d814149d86e6d50f5ec6daa254a0
Arvind Gouttumukkala <go_arvind@berkeley.edu>
2025-03-08 17:24:58 -0800

    type fix - AG

--- source/Score.py
+++ source/Score.py
@@ -2,7 +2,7 @@ import StructSim
 import SeqSim
 
 
-def Score(g: str, f: str, n: int, a: int = 0.5) -> tuple[str, float]:
+def Score(g: str, f: str, n: int, a: float = 0.5) -> tuple[str, float]:
     seq_scores = SeqSim.SeqSim(g, f, n)
     struc_score = StructSim.StructSim(list(seq_scores.keys()), f)
     total_score = {sub: (a * seq_scores[sub] + (1-a) * struc_score[sub]) for sub in struc_score.keys()}
